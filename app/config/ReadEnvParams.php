302e9d2c5e291f42627d2632cf2e809819271137
ovesco <guillaume.hochet@gmail.com>
2018-10-05 11:13:04 +0200

    swag env

--- app/config/ReadEnvParams.php
+++ app/config/ReadEnvParams.php
@@ -7,14 +7,6 @@ $container->setParameter('database_driver', 'pdo_mysql');
 
 if($vcapServices) {
 
-    $db = $vcapServices->{getenv('FOUNDRY_DB_NAME')}[0]->credentials;
-
-    $container->setParameter('database_host', $db->host);
-    $container->setParameter('database_port', $db->port);
-    $container->setParameter('database_name', $db->name);
-    $container->setParameter('database_user', $db->username);
-    $container->setParameter('database_password', $db->password);
-
     $container->setParameter('database_host', getenv('DB_HOST'));
     $container->setParameter('database_port', getenv('DB_PORT'));
     $container->setParameter('database_name', getenv('DB_NAME'));
@@ -44,4 +36,12 @@ if($vcapServices) {
     $container->setParameter('jwt_public_key_path', getenv('JWT_PUBLIC_KEY_PATH'));
     $container->setParameter('jwt_key_pass_phrase', getenv('JWT_KEY_PASS_PHRASE'));
     $container->setParameter('jwt_token_ttl', getenv('JWT_TOKEN_TTL'));
+
+    $db = $vcapServices->{getenv('FOUNDRY_DB_NAME')}[0]->credentials;
+
+    $container->setParameter('database_host', $db->host);
+    $container->setParameter('database_port', $db->port);
+    $container->setParameter('database_name', $db->name);
+    $container->setParameter('database_user', $db->username);
+    $container->setParameter('database_password', $db->password);
 }
