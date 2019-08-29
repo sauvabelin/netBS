f124946e464d2989efacb77c988259cba642ad1d
ovesco <guillaume.hochet@gmail.com>
2019-08-29 10:13:33 +0200

    factures list show creances names

--- src/Ovesco/FacturationBundle/Util/FactureListTrait.php
+++ src/Ovesco/FacturationBundle/Util/FactureListTrait.php
@@ -13,6 +13,7 @@ use NetBS\ListBundle\Column\DateTimeColumn;
 use NetBS\ListBundle\Column\SimpleColumn;
 use NetBS\ListBundle\Model\ListColumnsConfiguration;
 use Ovesco\FacturationBundle\Entity\Facture;
+use Ovesco\FacturationBundle\ListModel\Column\FactureCreancesColumn;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -45,11 +46,12 @@ trait FactureListTrait
             ])
             ->addColumn('Création', 'date', DateTimeColumn::class)
             ->addColumn("Dernière impression", 'latestImpression', DateTimeColumn::class)
-            ->addColumn('Montant', function(Facture $facture) { return $facture->getMontant() . " Chf."; }, SimpleColumn::class)
-            ->addColumn('Payé', function(Facture $facture) { return $facture->getMontantPaye() . " Chf."; }, SimpleColumn::class)
+            ->addColumn('Montant', function(Facture $facture) { return $facture->getMontant() . ".-"; }, SimpleColumn::class)
+            ->addColumn('Payé', function(Facture $facture) { return $facture->getMontantPaye() . ".-"; }, SimpleColumn::class)
             ->addColumn('Rappels', function(Facture $facture) {
                 return count($facture->getRappels());
             }, SimpleColumn::class)
+            ->addColumn('Creances', 'creances', FactureCreancesColumn::class)
             ->addColumn('Remarques', null, XEditableColumn::class, [
                 XEditableColumn::TYPE_CLASS => TextType::class,
                 XEditableColumn::PROPERTY => 'remarques',
